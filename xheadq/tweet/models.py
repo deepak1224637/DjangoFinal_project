e56126b3f00ee0fa1b7d85d7f9c8cd190f9e51f1
deepakkumar <deepak752820@gmail.com>
2025-06-20 18:11:46 +0530

    complete tweet_list.html page and tweet display

--- xheadq/tweet/models.py
+++ xheadq/tweet/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tex = models.TextField(max_length=240)
+    text = models.TextField(max_length=240)
     photo = models.ImageField(upload_to='photo/',
     blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,5 +14,5 @@ class Tweet(models.Model):
 
 
     def __str__(self):
-     return f'{self.user.username} - {self.tex[:10]}'
+     return f'{self.user.username} - {self.text[:10]}'
 
